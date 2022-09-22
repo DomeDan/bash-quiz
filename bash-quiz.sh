ecc2c63e67f93e8842561c6d9859fa94d1538d10
pyrit <pyrit@am-d053490932ef.intern>
2022-09-22 21:21:23 +0200

    fixed mpg123 again

--- bash-quiz.sh
+++ bash-quiz.sh
@@ -99,12 +99,8 @@ printf "$GREEN%s%*s\n"
 read -p "    Tryck Enter för att starta... " player_choice
 stty -echo
 clear
-<<<<<<< HEAD
-mpg123 ~/musik.mp3 > /dev/null 2>&1 &
-=======
 killall mpg123
 mpg123 -l 0 ~/musik.mp3 > /dev/null 2>&1 &
->>>>>>> 40379e3 (Fixed some mpg123 stuff)
 reset='\033[0m'
 BG='\033[47m'
 FG='\033[0;30m'
